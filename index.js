aeaab6e92d1fdd0824047dc4160c3e2e35cacd08
Tedd Angelo Gitalan <gitalan.tedd@gmail.com>
2023-11-08 14:50:52 +0800

    Second Commit

--- index.js
+++ index.js
@@ -4,22 +4,16 @@ dotenv.config();
 
 const { API_URL } = process.env;
 const app = Express();
-const router = Router();
 
-app.use(json()).use(
-  router.get("/", function (req, res) {
-    return res.send("Hello from rest-api v2");
-  }),
+//routers
+import userRouter from "./src/modules/user/user.route.js"; 
 
-  router.post("/post-request/", (req, res) => {
-    const body = req.body;
-    const params = req.params;
-    const queryParams = req.query;
-
-
-    res.status(401).json({ Body: body, Params: params, "Query Params": queryParams });
-  }),
-);
+app
+  .use(json())
+  .use(`${API_URL}users`, userRouter) //localhost:3000/api/v1/users
+  .use("*", (req, res) => {
+    return res.status(404).send("Not Found...");
+  });
 
 app.listen(3000, function () {
   console.log(API_URL);
